4425300e279536194a85e9585e925f0aa19de753
Tobias Schoch <tobias.schoch@vtxmail.ch>
2021-02-09 18:16:56 +0100

    use opencv for video capture

--- cam-reader/read_cam.py
+++ cam-reader/read_cam.py
@@ -1,17 +1,26 @@
 import socket
 import time
-from imutils.video import VideoStream
 import imagezmq
+import cv2
 import os
+import sys
 
 # Accept connections on all tcp addresses, port xxxx
 port = os.environ.get("ZMQ_PORT", "5553")
 sender = imagezmq.ImageSender(connect_to="tcp://*:{}".format(port), REQ_REP=False)
 
-rpi_name = socket.gethostname()  # send RPi hostname with each image
-picam = VideoStream(usePiCamera=True).start()
+rpi_name = "RadarPiCam"  # send RPi hostname with each image
+
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+if not ret:
+    print('Failed to open default camera. Exiting...')
+    sys.exit()
+cap.set(3, 640)
+cap.set(4, 480)
+
 time.sleep(2.0)  # allow camera sensor to warm up
 while True:  # send images until Ctrl-C
-    image = picam.read()
+    ret, image = cap.read()
     sender.send_image(rpi_name, image)
-    # The execution loop will continue even if no subscriber is connected
+    cv2.waitKey(100)
