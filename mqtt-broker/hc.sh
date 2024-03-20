f8432ac9e5aef1647485b0875cd0521fe95bfe97
Tobias Schoch <tobias.schoch@vtxmail.ch>
2024-03-20 14:09:44 +0100

    fix healthcheck on mqtt broker

--- mqtt-broker/hc.sh
+++ mqtt-broker/hc.sh
@@ -4,7 +4,7 @@ mosquitto_sub -h dietzi.ch -t '$SYS/broker/version' -u radar-pi -P "${MQTT_SERVE
 dietzi_broker_down="$?"
 # shellcheck disable=SC2016
 dietzi_bridge_ok_topic="\$SYS/broker/connection/${BALENA_DEVICE_NAME_AT_INIT}-bridge/state"
-dietzi_bridge_ok=$(mosquitto_sub -h 0.0.0.0 -t \'"$dietzi_bridge_ok_topic"\' -C 1 2> /dev/null)
+dietzi_bridge_ok=$(mosquitto_sub -h 0.0.0.0 -t "$dietzi_bridge_ok_topic" -C 1 2> /dev/null)
 
 # shellcheck disable=SC2181
 if [ "$?" -ne 0 ]; then
