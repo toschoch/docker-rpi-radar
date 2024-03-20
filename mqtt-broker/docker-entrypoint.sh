f943eb2b4b41ed280798b06f2a321032b9da6ee9
Tobias Schoch <tobias.schoch@vtxmail.ch>
2024-03-20 12:46:51 +0100

    remove verbosity and try to use a command instead cmd list

--- mqtt-broker/docker-entrypoint.sh
+++ mqtt-broker/docker-entrypoint.sh
@@ -6,8 +6,6 @@ set -e
 configfile="${CONFIG_FILE}"
 echo "expand variables in $configfile..."
 ./expand_vars.sh "$configfile"
-echo "config is now:"
-cat "$configfile"
 
 # Set permissions
 user="$(id -u)"
